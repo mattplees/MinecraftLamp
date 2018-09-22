a8038fc7990325b68f89b905d4f5b0ba4ead586d
Matt <matt@mlees.net>
2018-09-22 23:10:05 +0100

    updated scripts

--- jenkins/autopep8.sh
+++ jenkins/autopep8.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Running yapf"
-yapf -i -r *.py
+yapf -i -r code/*.py
 
 echo "Running reindent"
-find . -type f -name "*.py" | xargs python /usr/share/doc/python2.7/examples/Tools/scripts/reindent.py -r -n
+find ./code -type f -name "*.py" | xargs python /usr/share/doc/python2.7/examples/Tools/scripts/reindent.py -r -n
 
 echo "Running autopep8"
-autopep8 -i -r -aa *.py
+autopep8 -i -r -aa ./code/*.py
 
 echo "Running autoflake"
-autoflake --remove-all-unused-imports --remove-unused-variables -r -i *.py
+autoflake --remove-all-unused-imports --remove-unused-variables -r -i code/*.py
 
 echo "Running pep8ify"
-pep8ify -n -w .
+pep8ify -n -w code/
